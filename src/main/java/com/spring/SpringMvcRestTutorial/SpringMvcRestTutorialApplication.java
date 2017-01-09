c1e0c4f557f49a133aadf33c308e824ecc9456ef
Sachin.Karma <Sachin.Karma@IB-SEZ-PC-253.infobeans.in>
2017-01-09 16:44:45 +0530

    File Changed

--- src/main/java/com/spring/SpringMvcRestTutorial/SpringMvcRestTutorialApplication.java
+++ src/main/java/com/spring/SpringMvcRestTutorial/SpringMvcRestTutorialApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * 
- * @author Sachin.Karma
- * Spring Configuration file
+ * Spring boot Configuration file
  *
  */
 @SpringBootApplication
@@ -15,7 +14,5 @@ public class SpringMvcRestTutorialApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMvcRestTutorialApplication.class, args);
 	}
-	
-	//new line comment is added
-	//another new line is added
+
 }
