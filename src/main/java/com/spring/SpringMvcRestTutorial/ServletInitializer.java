fc6edb72072d5c6637c8d62bf93fef16e195dfa4
skkarma <sachinkkarma@gmail.com>
2017-01-09 15:48:41 +0530

    Project Added

---
+++ src/main/java/com/spring/SpringMvcRestTutorial/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.spring.SpringMvcRestTutorial;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringMvcRestTutorialApplication.class);
+	}
+
+}
---
+++ src/main/java/com/spring/SpringMvcRestTutorial/SpringMvcRestTutorialApplication.java
@@ -0,0 +1,12 @@
+package com.spring.SpringMvcRestTutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcRestTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcRestTutorialApplication.class, args);
+	}
+}
